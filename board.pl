d74adca01d7f54f08df93d38295cb0b9b7d620c3
João Carlos Santos <joaofloressantos@gmail.com>
2013-12-11 15:21:52 +0000

    Makes board and line and column restrictions

--- board.pl
+++ board.pl
@@ -1,7 +1,31 @@
+:-use_module(library(clpfd)).
+:-use_module(library(lists)).
 % create
 
-createBoard(B, S). %Creates board with areas already defined
+buildBoard([], N).
+buildBoard([H|T], N):-
+	length(H, N),
+	buildBoard(T,N).
 
-showGroups(B).
 
-printBoard(B).
+setupUnifiedList([], []).
+setupUnifiedList([H|T], R):-
+	setupUnifiedList(T, R1),
+	append(H, R1, R).
+
+setupDifferent([]).
+setupDifferent([H|T]):-
+	all_distinct(H),
+	setupDifferent(T).
+
+
+kenken(B, N) :-
+	length(B, N),
+	buildBoard(B, N),
+	setupUnifiedList(B, R), %mete todos os elementos do board numa só lista
+	domain(R, 1, N), %todos os elementos têm que ser números de 1 a 9
+	setupDifferent(B), %mete as colunas com numeros diferentes
+	transpose(B, NB), %faz a transposta para agora mexer nas linhas
+	setupDifferent(NB), %linhas com números todos diferentes
+	setupUnifiedList(B, R1), %volta a juntar tudo para o labeling
+	labeling([], R1).
--- kenken.pl
+++ kenken.pl
@@ -2,6 +2,4 @@ play.
 
 checkEnd(B):-
 
-+
-3
 P1 + P2 + P3 #< 9+9+9
